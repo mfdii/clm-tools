3b0441530a4be8a928b1f338d0b3c779c44ecb9a
Michael Ducy <michael@ducy.org>
2013-02-13 10:19:26 -0500

    added run-instance, describe-tenants, and clean up

--- lib/clm.py
+++ lib/clm.py
@@ -21,49 +21,87 @@ class CLM:
 
 	def describeInstances(self):
 		http=httplib2.Http()
-                request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
-                response, content = http.request(self.endpoint+"/csm/serviceofferinginstance/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
-		return content
+		request = {"callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1}
+		response, content = http.request(self.endpoint+"/csm/serviceofferinginstance/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		json_content = loads(content)
+
+		return json_content[0]['results']
 
 	def describeImages(self):
 		http=httplib2.Http()
-                request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
-                response, content = http.request(self.endpoint+"/csm/serviceoffering/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
-                json_content = loads(content)
-		
+		request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
+		response, content = http.request(self.endpoint+"/csm/serviceoffering/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		json_content = loads(content)
+
 		return json_content[0]['results']
-	
-	
+
+
 	def describeRegions(self):
 		http=httplib2.Http()
-                request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
-                response, content = http.request(self.endpoint+"/csm/Location/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
-                json_content = loads(content)
-
+		request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
+		response, content = http.request(self.endpoint+"/csm/Location/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		json_content = loads(content)
 		return json_content[0]['results']
 
 	def describeAZs(self):
 		http=httplib2.Http()
-                request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
-                response, content = http.request(self.endpoint+"/csm/Pod/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
-                json_content = loads(content)
+		request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
+		response, content = http.request(self.endpoint+"/csm/Pod/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		json_content = loads(content)
 
 		return json_content[0]['results']
 
 	def describeVPCs(self):
 		http=httplib2.Http()
-                request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
-                response, content = http.request(self.endpoint+"/csm/NetworkContainer/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
-                json_content = loads(content)
+		request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
+		response, content = http.request(self.endpoint+"/csm/NetworkContainer/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		json_content = loads(content)
 		
 		return json_content[0]['results']
 
-	def runInstance(self,SOreConID):
+	def describeTenants(self):
+		http=httplib2.Http()
+		request =  { "callbackURL":"", "operationParams": [ ], "postCallout":"", "preCallout":"", "timeout":-1  }
+		response, content = http.request(self.endpoint+"/csm/Organization/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		json_content = loads(content)
+		
+		return json_content[0]['results']
+
+	def getTenant(self,tenant):
+		http=httplib2.Http()
+		response, content = http.request(self.endpoint+"/csm"+tenant, "GET",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken})
+		json_content = loads(content)
+		return json_content[0]
+
+	def runInstance(self,SOreConID, tenant, quantity=1):
 
 		http=httplib2.Http()
-                request =  { "callbackURL":"", "operationParams": [{ "quantity": 1, "serviceOfferingID": SOreConID }], "postCallout":"", "preCallout":"", "timeout":-1  }
-                response, content = http.request(self.endpoint+"/csm/NetworkContainer/search", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
-#                json_content = loads(content)
-#                for serviceOffering in  json_content[0]['results']:
-#                        print serviceOffering['guid'] + "\t" + serviceOffering['name']
+		request =  { "callbackURL":"", "operationParams": [{
+				"name" : "serviceOfferingID",
+				"value" : {
+					"cloudClass" : "com.bmc.cloud.model.beans.ServiceOffering",
+  					"reconciliationID" : SOreConID
+				},
+				"type" : "com.bmc.cloud.model.beans.ServiceOffering",
+				"multiplicity" : "1"
+				}, {
+				"name" : "quantity",
+				"value" : quantity,
+				"type" : "java.lang.Integer",
+				"multiplicity" : "1"
+				},{
+				"name" : "name",
+				"value" : "centos-api",
+				"type" : "java.lang.String",
+				"multiplicity" : "0..1"
+				},{
+				"name" : "tenant",
+				"value" : tenant,
+				"type" : "java.lang.String",
+				"multiplicity" : "0..1"
+				}
+				], "postCallout":"", "preCallout":"", "timeout":-1  }
+		response, content = http.request(self.endpoint+"/csm/ServiceOfferingInstance/bulkCreate", "POST",headers={'Accept': 'application/json','User-agent': 'bmc-tools', 'Authentication-Token': self.authToken}, body=dumps(request, indent=2))
+		print dumps(request, indent=2)
+		print response
 		print content
